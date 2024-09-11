d252cadf06815d7112a7de50a50af563b9e8a8a2
MaduFurini <madufurini@gmail.com>
2024-09-11 13:06:55 -0300

    Added question description in question view

--- database/database.js
+++ database/database.js
@@ -1,6 +1,6 @@
 const sequelize = require('sequelize');
 
-const connection = new sequelize('projetopratico', 'root', '1234', {
+const connection = new sequelize('projetopratico', 'root', 'S3mpher@0102', {
     host: 'localhost',
     dialect: 'mysql'
 });
